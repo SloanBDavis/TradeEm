7d75a10d99e8622c180caf734adc5dfdd72ecc6d
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-01-28 13:37:09 -0600

    got flask app to work

--- backend/flask-app/app.py
+++ backend/flask-app/app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='../../frontend/public')
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='../../frontend/build/static', static_url_path='/static')
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def serve():
+    return send_from_directory('../../frontend/build', 'index.html')
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('../../frontend/build/static', path)
+
+@app.route('/manifest.json')
+def serve_manifest():
+    return send_from_directory('../../frontend/build', 'manifest.json')
+
+@app.route('/logo192.png')
+def serve_logo():
+    return send_from_directory('../../frontend/build', 'logo192.png')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
