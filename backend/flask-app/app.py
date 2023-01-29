d9c03e4908f801c6f38148f9204a16daf26fd3bf
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-01-28 21:26:58 -0600

    add database update

--- backend/flask-app/app.py
+++ backend/flask-app/app.py
@@ -1,25 +1,48 @@
 from flask import Flask, send_from_directory
+import psycopg2
 
-app = Flask(__name__, static_folder='../../frontend/build/static', static_url_path='/static')
+app = Flask(
+    __name__, static_folder="../../frontend/build/static", static_url_path="/static"
+)
 
-@app.route('/')
+
+@app.route("/")
 def serve():
-    return send_from_directory('../../frontend/build', 'index.html')
+    return send_from_directory("../../frontend/build", "index.html")
 
-@app.route('/static/<path:path>')
+
+@app.route("/static/<path:path>")
 def serve_static(path):
-    return send_from_directory('../../frontend/build/static', path)
+    return send_from_directory("../../frontend/build/static", path)
 
-@app.route('/manifest.json')
+
+@app.route("/manifest.json")
 def serve_manifest():
-    return send_from_directory('../../frontend/build', 'manifest.json')
+    return send_from_directory("../../frontend/build", "manifest.json")
 
-@app.route('/logo192.png')
+
+@app.route("/logo192.png")
 def serve_logo():
-    return send_from_directory('../../frontend/build', 'logo192.png')
+    return send_from_directory("../../frontend/build", "logo192.png")
 
 
-if __name__ == '__main__':
+@app.route("/updateDB", methods=["POST"])
+def update_db():
+    data = request.form
+
+    conn = psycopg2.connect(
+        host="tradem.cgtrc7tayjzx.us-east-1.rds.amazonaws.com",
+        port=3306,
+        user="admin",
+        password="Simonishot",
+        database="tradem",
+    )
+
+    cur = conn.cursor()
+    cur.execute("INSERT INTO live_trades(username, wants, haves, location) VALUES({}, {}, {}, {})".format(data['username'], data['wants'], data['lol'], data['location']))
+    results = cur.fetchall()
+    return results
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-
-
